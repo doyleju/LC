30879aad915dada524ed55577ef9f9d068cf4274
doyleju <36811768+doyleju@users.noreply.github.com>
2018-07-28 20:52:20 +0100

    Update 191_number_1_bits.py

--- bit_manip/191_number_1_bits.py
+++ bit_manip/191_number_1_bits.py
@@ -18,4 +18,17 @@ class Solution(object):
         """
         
         #2 79p
-        return bin(n).count('1')
+        #return bin(n).count('1')
+    
+        #3 79p
+        mask = 1
+        bits = 0
+        
+        for i in range(32):
+            #n = n & mask
+            if n & mask != 0:
+                bits += 1
+                
+            mask = mask << 1
+                
+        return bits
