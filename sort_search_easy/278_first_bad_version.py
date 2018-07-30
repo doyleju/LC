487728b0dc7820dceb08b0c97f9e0c342ab485d6
doyleju <36811768+doyleju@users.noreply.github.com>
2018-07-30 11:16:25 +0100

    Update 278_first_bad_version.py

--- sort_search_easy/278_first_bad_version.py
+++ sort_search_easy/278_first_bad_version.py
@@ -21,3 +21,21 @@ class Solution(object):
             mid = (start + end) // 2
             
         return lowest_bad
+    
+        """
+        # Site Solution
+        # Note overflow prevention?
+        public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+        }
+        """
