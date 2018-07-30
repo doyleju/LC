d9bcb49f2ec317ef065a2c4dbae4a67f7c35be21
doyleju <36811768+doyleju@users.noreply.github.com>
2018-07-30 15:01:17 +0100

    Create 104_maximum_depth_binary_tree.py

---
+++ trees_easy/104_maximum_depth_binary_tree.py
@@ -0,0 +1,57 @@
+class Solution:
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        
+        """
+        #1 70p
+        # Recursive
+        #if not root:
+        #    return 0
+        
+        return self.dfs(root, 0)
+        
+        
+    def dfs(self, node, result):
+        
+        if not node:
+            return result
+        
+        return max(self.dfs(node.left, result + 1), self.dfs(node.right, result + 1))
+        """
+    
+        #2 70p
+        # Recursive    
+        
+        def dfs(node, result):
+        
+            if not node:
+                return result
+        
+            return max(dfs(node.left, result + 1), dfs(node.right, result + 1))
+
+        return dfs(root, 0)
+        
+        """
+        #3
+        # DFS
+        if not root:
+            return 0
+        nodes = [root]
+        ans = 0
+        while nodes:
+            temp = []
+            ans += 1
+            for node in nodes:
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+            if temp:
+                nodes = temp
+            else:
+                nodes = []
+        return ans
+        """
