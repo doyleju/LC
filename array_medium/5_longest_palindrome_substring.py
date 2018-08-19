9f26546b1c0e8dc25762189f3d7b04661fadd0fb
doyleju <36811768+doyleju@users.noreply.github.com>
2018-08-19 15:02:53 +0100

    Create 5_longest_palindrome_substring.py

---
+++ array_medium/5_longest_palindrome_substring.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    # 83p
+    def longestPalindrome(self, s):
+        if (s==s[::-1] or len(s) < 2):
+            return s
+        res = ""
+        for i in range(len(s)):
+            # odd e.g. aba
+            tmp = self.helper(s, i, i)
+            if len(tmp) > len(res):
+                res = tmp
+            # even e.g abba
+            tmp = self.helper(s, i, i+1)
+            if len(tmp) > len(res):
+                res = tmp
+            #if (i>(len(s)//2) and len(res)>(len(s)//2)):
+            #    break
+        return res
+ 
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1; r += 1
+        return s[l+1:r]
